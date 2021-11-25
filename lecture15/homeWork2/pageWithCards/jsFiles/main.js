d44ea453d6c6dbb3c0e02d7461f830beb5e7920a
kiril <kirilivailoviliev@abv.bg>
2021-11-25 23:05:27 +0200

    HomeWork15 - Card game animation.

---
+++ lecture15/homeWork2/pageWithCards/jsFiles/main.js
@@ -0,0 +1,24 @@
+function reload() {
+    return location.reload();
+};
+function cardTurnOn(params) {
+   var domeNote = document.getElementsByClassName("card");
+};
+
+
+
+
+// var playPause = anime({
+//     targets: '.span.div.card.firstCard',
+//     translateY: [
+//         // { value: 160, duration: 1200 },
+//         { value: 0, duration: 800 },
+//     ],
+//     rotateY: {
+//         value: "0.5turn",
+//         easing: "easeInOutSine"
+//     },
+//     delay: function (el, i, l) { return i * 1700 },
+//     autoplay: false,
+// });
+// document.querySelector('.first.firstCard').onclick = playPause.play;
